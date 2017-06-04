4b831bb472f2ff46a4f94733b7fc45c2868fff3a
이동혁 <knulps@idonghyeogui-MacBook-Pro.local>
2017-06-05 01:12:06 +0900

    코드 정리

--- app/src/main/java/com/momu/tale/activity/PinLockActivity.java
+++ app/src/main/java/com/momu/tale/activity/PinLockActivity.java
@@ -25,16 +25,16 @@ import butterknife.ButterKnife;
  */
 
 public class PinLockActivity extends AppCompatActivity {
-
     @BindView(R.id.pin_lock_view) PinLockView mPinLockView;
     @BindView(R.id.indicator_dots) IndicatorDots indicatorDots;
     @BindView(R.id.txt_header) TextView txtHeader;
 
     Context mContext = this;
+    Vibrator vibrator;
+
     boolean isLockMode = false; //잠금 확인모드인지 여부
     boolean isCheckAgain = false;   //비밀번호 재확인 단계인지 여부
     String pinNumber = null;
-    Vibrator vibrator;
 
     private static final String TAG = "PinLockActivity";
 
@@ -44,9 +44,23 @@ public class PinLockActivity extends AppCompatActivity {
         overridePendingTransition(0, 0);
         setContentView(R.layout.activity_pinlock);
         ButterKnife.bind(this);
+
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
-        if (getIntent().getBooleanExtra("isLockMode", false)) {  //잠금 모드인 경우
+        mPinLockView.attachIndicatorDots(indicatorDots);
+        mPinLockView.setPinLockListener(mPinLockListener);
+
+        initLayout();
+    }
+
+    /**
+     * 모드에 따른 레이아웃 초기화
+     * 1. 잠금 해제 모드
+     * 2. 잠금 비밀번호 재확인 모드
+     * 3. 잠금 비밀번호 첫설정 혹은 변경 모드
+     */
+    void initLayout() {
+        if (getIntent().getBooleanExtra("isLockMode", false)) {  //잠금 해제 모드인 경우
             txtHeader.setText(R.string.lock_pw_insert);
             isLockMode = true;
 
@@ -58,10 +72,6 @@ public class PinLockActivity extends AppCompatActivity {
         } else {    //잠금 비밀번호 첫설정 혹은 변경인 경우
             txtHeader.setText(R.string.lock_pw_insert);
         }
-
-        indicatorDots.setPinLength(4);
-        mPinLockView.attachIndicatorDots(indicatorDots);
-        mPinLockView.setPinLockListener(mPinLockListener);
     }
 
     private PinLockListener mPinLockListener = new PinLockListener() {
@@ -71,19 +81,19 @@ public class PinLockActivity extends AppCompatActivity {
             indicatorDots.setPinLength(pin.length());
 
             if (isLockMode) {    //비밀번호 잠금 해제모드인 경우
-                if (pin.equals(AppPreference.loadScreenPinNumber(mContext))) {  //비밀번호 일치
-                    if (getIntent().getBooleanExtra("isStartApplication", false)) {  //앱 처음 시작인 경우
+                if (pin.equals(AppPreference.loadScreenPinNumber(mContext))) {  //비밀번호 일치한 경우
+                    if (getIntent().getBooleanExtra("isStartApplication", false)) {  //앱 처음 시작인 경우 메인 엑티비티 실행해 준다.
                         //메인 페이지 시작
                         Intent intent = new Intent(mContext, MainActivity.class);
                         intent.putExtra("fragmentName", MainActivity.MAIN_FRAGMENT_MAIN);
                         startActivity(intent);
                     }
 
-                } else {
+                } else {    //비밀번호 틀린 경우
                     Toast.makeText(mContext,R.string.toast_lock_pw_incorrect, Toast.LENGTH_SHORT).show();
 
                     vibrator.vibrate(500);
-                    Intent lockIntent = new Intent(getApplicationContext(), PinLockActivity.class);
+                    Intent lockIntent = new Intent(mContext, PinLockActivity.class);
                     lockIntent.putExtra("isLockMode", true);
 
                     if (getIntent().getBooleanExtra("isStartApplication", false)) {  //앱 처음 시작인 경우
@@ -94,13 +104,11 @@ public class PinLockActivity extends AppCompatActivity {
                 }
 
             } else if (isCheckAgain) {  //비밀번호 재확인인 경우
-                if (pinNumber != null && pinNumber.equals(pin)) {
-
+                if (pinNumber != null && pinNumber.equals(pin)) {   //핀넘버 일치
                     Toast.makeText(mContext, R.string.toast_lock_setting_done, Toast.LENGTH_SHORT).show();
                     AppPreference.saveScreenPinNumber(mContext, pin);
 
-                } else {
-
+                } else {    //핀넘버 틀림
                     Toast.makeText(mContext, R.string.toast_lock_pw_incorrect, Toast.LENGTH_SHORT).show();
                     vibrator.vibrate(500);
 
@@ -108,7 +116,7 @@ public class PinLockActivity extends AppCompatActivity {
                     startActivity(lockIntent);
                 }
 
-            } else {    //비밀번호 처음 입력인 경우 재확인을 해 주어야 한다.
+            } else {    //비밀번호 처음 입력인 경우 재확인을 위해 잠금화면 다시 보여줌
                 Intent checkAgainIntent = new Intent(mContext, PinLockActivity.class);
                 checkAgainIntent.putExtra("pinNumber", pin);
                 checkAgainIntent.putExtra("checkAgain", true);
@@ -126,18 +134,18 @@ public class PinLockActivity extends AppCompatActivity {
         @Override
         public void onPinChange(int pinLength, String intermediatePin) {
             LogHelper.e(TAG, "Pin changed, new length " + pinLength + " with intermediate pin " + intermediatePin);
-//            indicatorDots.setPinLength(intermediatePin.length());
         }
     };
 
     @Override
     public void onBackPressed() {
-        if (isLockMode) {
+        if (isLockMode) {   //잠금 해제 화면인 경우 백버튼을 누르면 홈버튼 누른 효과 나도록 한다.
             Intent startMain = new Intent(Intent.ACTION_MAIN);
             startMain.addCategory(Intent.CATEGORY_HOME);
             startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(startMain);
-        } else {
+
+        } else {    //잠금 비밀번호 설정 화면인 경우
             finish();
         }
     }
